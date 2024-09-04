bea5e085396f79b0f971291629987a1ddb5df7e1
Kenil Gondaliya <kenilgondaliya960gmail.com>
2024-09-04 20:00:49 +0530

    utils

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,7 +6,16 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+    app.Listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.chatch((err) => {
+    console.log("MONGO db Connection failed !!!", err);
+    
+})
 
 
 
